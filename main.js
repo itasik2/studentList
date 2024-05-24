762f0b022041c48c9b5e2f5529264c6e15d74d7a
Виталий Боляк <itasik2@mail.ru>
2024-05-25 04:40:24 +0500

    update

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const serverClientsUrl = 'http://localhost:3000/api/clients/';
 
-// Добавить клиента
 async function serverAddClient(obj) {
     const response = await fetch(serverClientsUrl, {
         method: "POST",
@@ -9,132 +8,79 @@ async function serverAddClient(obj) {
         },
         body: JSON.stringify(obj)
     });
-    let data = await response.json();
-    return data
-};
 
-// Получить клиента
+    const data = await response.json();
+    console.log('Добавленный клиент:', data);
+    return data;
+}
+
 async function serverGetClient() {
     const response = await fetch(serverClientsUrl, {
         method: "GET",
         headers: {
             'Content-Type': 'application/json'
-        },
+        }
     });
-    let data = await response.json();
+
+    const data = await response.json();
+    console.log('Полученные клиенты:', data);
     return data;
-};
-// удаление клиента
+}
+
 async function serverDeleteClient(id) {
-    const response = await fetch(serverClientsUrl + id, {
-        method: "DELETE",
-    })
-    let data = await response.json();
+    const response = await fetch(`${serverClientsUrl}${id}`, {
+        method: "DELETE"
+    });
+
+    const data = await response.json();
+    console.log('Удаленный клиент:', data);
     return data;
-};
-// изменение клиента
-async function serverEditClient(id) {
-    const response = await fetch(serverClientsUrl + 'client' + id, {
-        method: "POST",
+}
+
+async function serverEditClient(id, obj) {
+    const response = await fetch(`${serverClientsUrl}${id}`, {
+        method: "PATCH",
         headers: {
             'Content-Type': 'application/json'
         },
+        body: JSON.stringify(obj)
     });
-    let data = await response.json();
+
+    const data = await response.json();
+    console.log('Измененный клиент:', data);
     return data;
 }
 
 let serverDataClient = await serverGetClient();
-// массив клиентов
-let clientsArr = [{
-    // ID клиента, заполняется сервером автоматически, после создания нельзя изменить
-    id: '1234567890',
-    // дата и время создания клиента, заполняется сервером автоматически, после создания нельзя изменить
-    createdAt: '2021-02-03T13:07:29.554Z',
-    // дата и время изменения клиента, заполняется сервером автоматически при изменении клиента
-    updatedAt: '2021-02-03T13:07:29.554Z',
-    // * обязательное поле, имя клиента
-    name: 'Василий',
-    // * обязательное поле, фамилия клиента
-    surname: 'Пупкин',
-    // необязательное поле, отчество клиента
-    lastName: 'Васильевич',
-    // контакты - необязательное поле, массив контактов
-    // каждый объект в массиве (если он передан) должен содержать непустые свойства type и value
-    contacts: [{
-            type: 'Телефон',
-            value: '+71234567890'
-        },
-        {
-            type: 'Email',
-            value: 'abc@xyz.com'
-        },
-        {
-            type: 'Facebook',
-            value: 'https://facebook.com/vasiliy-pupkin-the-best'
-        }
-    ]
-}];
-// если дата база не пуста то данные прел\даются в массив
-if (!serverDataClient) {
-    clientsArr = serverDataClient
-};
+let clientsArr = [];
 
-let clientsTable = document.getElementById('clients-table')
+if (serverDataClient) {
+    clientsArr = serverDataClient;
+}
 
-// конкатенирует фамилию, имя и отчество
+let clientsTable = document.getElementById('clients-table'),
+    formWrap = document.getElementById('form-wrap'),
+    addForm = document.getElementById('add-client'),
+    addBtn = document.getElementById('add-btn'),
+    formTitle = document.getElementById('modal-form-title'),
+    addContactBtn = document.getElementById('add-contact-btn')
+
+
 function getFio(surname, name, lastname) {
-    return surname + ' ' + name + ' ' + lastname
+    return `${surname} ${name} ${lastname}`;
 }
-
-function getTD(text) {
-    let td = document.createElement('td');
-    td.textContent = text;
-    return td;
-}
-
-function getBtn(text, className) {
-    const btn = document.createElement('button');
-    btn.textContent = text;
-    btn.classList.add(className);
-    return btn
-}
-
-// function render() {
-//     clientsTable.innerHTML = ''
-//     let newArr = [...clientsArr];
-
-//     for (let i of newArr) {
-//         let clientTR = document.createElement('tr'),
-//             idTD = getTD(i.id),
-//             fioTD = getTD(getFio(i.surname, i.name, i.lastname)),
-//             createTimeTD = getTD(i.createAT),
-//             updateTimeTD = getTD(i.updateAT),
-//             contactsTD = getTD(i.contacts),
-//             actionsTD = document.createElement('td'),
-//             editBtn = getBtn('Изменить', 'editBtn'),
-//             deleteBtn = getBtn('Удалить', 'deleteBtn')
-
-
-//         deleteBtn.addEventListener('click', async function () {
-//             await serverDeleteClient(i.id);
-//             clientTR.remove
-//         })
-
-//         actionsTD.append(editBtn, deleteBtn);
-//         clientTR.append(idTD, fioTD, createTimeTD, updateTimeTD, contactsTD, actionsTD);
-//         clientsTable.append(clientTR)
-//     }
-// }
 
 function getContacts(contacts) {
     return contacts.map(contact => `${contact.type}: ${contact.value}`).join(', ');
 }
 
+
+
+
+
 function render() {
     clientsTable.innerHTML = '';
-    let newArr = [...clientsArr]
-    newArr.forEach(client => {
+    clientsArr.forEach(client => {
         const clientTR = document.createElement('tr');
         clientTR.innerHTML = `
             <td>${client.id}</td>
@@ -143,58 +89,96 @@ function render() {
             <td>${client.updatedAt}</td>
             <td>${getContacts(client.contacts)}</td>
             <td>
-                <button class="editBtn">Изменить</button>
-                <button class="deleteBtn">Удалить</button>
+                <button class="editBtn" data-id="${client.id}">Изменить</button>
+                <button class="deleteBtn" data-id="${client.id}">Удалить</button>
             </td>
         `;
 
         const deleteBtn = clientTR.querySelector('.deleteBtn');
         deleteBtn.addEventListener('click', async function () {
-            await serverDeleteClient(client.id)
-            clientTR.remove;
-            clientsTable.innerHTML = '';
+            await serverDeleteClient(client.id);
+            clientsArr = clientsArr.filter(c => c.id !== client.id);
+            render();
         });
 
+        const editBtn = clientTR.querySelector('.editBtn');
+        editBtn.addEventListener('click', function () {
+            fillForm(client);
+            formWrap.classList.remove('d-none');
+            addBtn.dataset.editId = client.id;
+        });
 
         clientsTable.append(clientTR);
-
     });
-
 }
 
-document.getElementById('add-client').addEventListener('submit', async function (event) {
+function fillForm(client) {
+    document.getElementById('surname').value = client.surname;
+    document.getElementById('name').value = client.name;
+    document.getElementById('lastName').value = client.lastName || '';
+    if (client.contacts && client.contacts.length > 0) {
+        document.getElementById('form-select').value = client.contacts[0].type;
+        document.getElementById('form-select-input').value = client.contacts[0].value;
+    }
+}
+
+
+addContactBtn.addEventListener('click', function (event) {
     event.preventDefault();
+    let count = +1;
+});
+
+addForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+
 
 
     const surname = document.getElementById('surname').value.trim();
     const name = document.getElementById('name').value.trim();
-    const lastname = document.getElementById('lastName').value.trim();
+    const lastName = document.getElementById('lastName').value.trim();
+
     const contactsType = document.getElementById('form-select').value;
-    const ContactsVlue = document.getElementById('form-select-input').value
+    const contactsValue = document.getElementById('form-select-input').value.trim();
+
     const contacts = [{
-        contactsType,
-        ContactsVlue
+        type: contactsType,
+        value: contactsValue
     }];
 
-    let clientObj = {
-        id: this.id,
-        createdAt: this.createdAt,
-        updatedAt: this.updatedAt,
-        surname: surname,
-        name: name,
-        lastname: lastname,
-        contacts: contacts
+
+    const clientObj = {
+        surname,
+        name,
+        lastName,
+        contacts
+    };
+
+
+
+
+
+    if (addBtn.dataset.editId) {
+        const clientId = addBtn.dataset.editId;
+        const clientDataObj = await serverEditClient(clientId, clientObj);
+        const index = clientsArr.findIndex(c => c.id === clientId);
+        clientsArr[index] = clientDataObj;
+        // addBtn.textContent = "Добавить";
+        // delete addBtn.dataset.editId;
+    } else {
+        const clientDataObj = await serverAddClient(clientObj);
+        clientsArr.push(clientDataObj);
     }
+    render();
+    formWrap.classList.add('d-none');
 
-    let clientDataObj = await serverAddClient(clientObj);
+    event.target.reset();
+});
 
+addBtn.addEventListener('click', function () {
+    formWrap.classList.remove('d-none');
+    formTitle.textContent = 'Новый клиент'
+});
 
-    clientsArr.push(clientDataObj);
-
-    //очищает форму перед отрисовкой
-    event.target.innerHTML = '';
-    render()
-
-})
-render()
+render();
 console.log(clientsArr);
